f64d81d8634a416f3bfe2c18e09d83c7dc429e72
harsha1906-an <harshuharsha73@gmail.com>
2026-02-09 17:42:48 +0530

    feat: remove debug code from InstallApp component

--- frontend/src/components/InstallApp/index.jsx
+++ frontend/src/components/InstallApp/index.jsx
@@ -7,11 +7,9 @@ const InstallApp = () => {
     const [showInstructions, setShowInstructions] = useState(false);
 
     useEffect(() => {
-        console.log('InstallApp component mounted');
         const handler = (e) => {
             e.preventDefault();
             setDeferredPrompt(e);
-            console.log('beforeinstallprompt event captured');
         };
 
         window.addEventListener('beforeinstallprompt', handler);
@@ -25,11 +23,6 @@ const InstallApp = () => {
         if (deferredPrompt) {
             deferredPrompt.prompt();
             deferredPrompt.userChoice.then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the install prompt');
-                } else {
-                    console.log('User dismissed the install prompt');
-                }
                 setDeferredPrompt(null);
             });
         } else {
@@ -40,28 +33,10 @@ const InstallApp = () => {
 
     return (
         <>
-            <div style={{
-                position: 'fixed',
-                top: '100px',
-                right: '10px',
-                background: 'red',
-                color: 'white',
-                padding: '10px',
-                zIndex: 9999,
-                border: '3px solid yellow'
-            }}>
-                DEBUG: InstallApp Loaded!
-            </div>
             <Button
                 type="primary"
                 icon={<DownloadOutlined />}
                 onClick={handleInstallClick}
-                data-testid="install-app-button"
-                style={{ 
-                    display: 'block',
-                    visibility: 'visible',
-                    zIndex: 1000
-                }}
             >
                 Install App
             </Button>
